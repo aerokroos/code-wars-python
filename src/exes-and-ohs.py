17fddc634d4b0f1884ae664be8f98ba0485f59ee
Alberto Ornelas <albertoornelass44@gmail.com>
2021-05-02 17:24:49 -0700

    exes-and-ohs

---
+++ src/exes-and-ohs.py
@@ -0,0 +1,33 @@
+# Check to see if a string has the same amount of 'x's and 'o's.
+# The method must return a boolean and be case insensitive.
+# The string can contain any char.
+
+# Examples input/output:
+
+# XO("ooxx") => true
+# XO("xooxx") => false
+# XO("ooxXm") => true
+# XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+# XO("zzoo") => false
+
+# split woord to characters
+def xo(string):
+    x_arr = []
+    o_arr = []
+    match = False
+
+    for char in string:
+        if (char == "x" or char == "X"):
+            x_arr.append(char)
+        elif (char == "o" or char == "O"):
+            o_arr.append(char)
+        elif(char == "0"):
+            match = True
+
+    if (len(x_arr) == len(o_arr)):
+        match = True
+
+    if (match):
+        return match
+    else:
+        return match
